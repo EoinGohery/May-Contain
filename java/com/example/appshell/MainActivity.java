0bae3e16df73726857a2eb574bc6c434c359aac4
CianMc98 <38212861+CianMc98@users.noreply.github.com>
2019-11-23 15:59:47 +0000

    added language but commented out previous

--- java/com/example/appshell/MainActivity.java
+++ java/com/example/appshell/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.appshell;
+/*package com.example.appshell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +14,89 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void OnLoginClick(View view) {
+        // login popup
+        // that jumps onto next activity which is account settings
+    }
+
+}
+*/
+package com.example.appshell;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadLocale();
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+        Button changeLang = findViewById(R.id.changLang);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLanguageDialog();
+            }
+        })
+        ;
+    }
+
+    private void showChangeLanguageDialog() {
+        final String[] listItems = {"French", "English"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("@/chooseLang");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(i ==0) {
+                    setLocale("fr");
+                    recreate();
+                }
+                if(i ==0) {
+                    setLocale("en");
+                    recreate();
+                }
+                dialog.dismiss();
+            }
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
+
+    }
 
     public void OnLoginClick(View view) {
         // login popup
