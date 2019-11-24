e5f6da517f1f6790c9ac6960c1da0c1bf469dee5
CianMc98 <38212861+CianMc98@users.noreply.github.com>
2019-11-24 18:31:19 +0000

    commented out previous, put in shared preferences which saves value after checking, should work fine

--- app/src/main/java/com/c17206413/maycontain/AccountSettings.java
+++ app/src/main/java/com/c17206413/maycontain/AccountSettings.java
@@ -1,4 +1,4 @@
-package com.c17206413.maycontain;
+/* package com.c17206413.maycontain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -60,6 +60,93 @@ public class AccountSettings extends AppCompatActivity {
                 break;
 
         }
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+}
+*/
+
+package com.c17206413.maycontain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class AccountSettings extends AppCompatActivity {
+
+    CheckBox check1, check2, check3;
+    Button button_sel;
+
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    final SharedPreferences.Editor editor = preferences.edit();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+
+        check1 = findViewById(R.id.nutAllergyCheck);
+        check2 = findViewById(R.id.dairyAllergyCheck);
+        check3 = findViewById(R.id.glutenAllergyCheck);
+        button_sel = findViewById(R.id.acceptClickBox);
+
+
+        button_sel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = "@string/SelectedAllergens";
+                if (check1.isChecked() && preferences.getBoolean("checked",false)== true) {
+                    result += "@string/nutAllergy";
+                }
+                if (check2.isChecked() && preferences.getBoolean("checked",false)== true) {
+                    result += "@string/dairyAllergy";
+                }
+                if (check3.isChecked()&& preferences.getBoolean("checked",false)== true) {
+                    result += "@string/gluten_Allergy";
+                }
+
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+            }
+
+    });
+
+    }
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+        String str="";
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.nutAllergyCheck:
+                str = checked?"@string/nutAllergySelected":"@string/nutAllergyDeselected";
+                editor.putBoolean("checked", true);
+                editor.apply();
+                break;
+            case R.id.dairyAllergyCheck:
+                str = checked?"@string/dairySelect":"@string/dairyDeselect";
+                editor.putBoolean("checked", true);
+                editor.apply();
+                break;
+            case R.id.glutenAllergyCheck:
+                str = checked?"@string/glutenSel":"@string/glutenDesel";
+                editor.putBoolean("checked", true);
+                editor.apply();
+                break;
+
+        }
+
+
         Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
 
 
