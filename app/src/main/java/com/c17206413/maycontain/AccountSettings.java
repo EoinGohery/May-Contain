cda960bcde9c7badd08f334f32e7073f33132b8b
CianMc98 <38212861+CianMc98@users.noreply.github.com>
2019-11-27 14:56:21 +0000

    added language in this activity, not sure if ln 109 passes pref to main

--- app/src/main/java/com/c17206413/maycontain/AccountSettings.java
+++ app/src/main/java/com/c17206413/maycontain/AccountSettings.java
@@ -1,24 +1,34 @@
 package com.c17206413.maycontain;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.*;
+
+import java.util.Locale;
 
 public class AccountSettings extends AppCompatActivity {
 
     CheckBox check1, check2, check3;
     Button button_sel;
 
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Button changeLanguageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_account_settings);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         editor = preferences.edit();
@@ -28,18 +38,21 @@ public class AccountSettings extends AppCompatActivity {
         check3 = findViewById(R.id.glutenAllergyCheck);
         button_sel = findViewById(R.id.saveBox);
 
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle(getResources().getString(R.string.app_name));
+
 
         button_sel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String result = "@string/SelectedAllergens";
-                if (check1.isChecked() &&preferences.getBoolean(getString(R.string.checked), false)) {
+                if (check1.isChecked() && preferences.getBoolean(getString(R.string.checked), false)) {
                     result += "@string/nutAllergy";
                 }
                 if (check2.isChecked() && preferences.getBoolean(getString(R.string.checked), false)) {
                     result += "@string/dairyAllergy";
                 }
-                if (check3.isChecked()&& preferences.getBoolean(getString(R.string.checked), false)) {
+                if (check3.isChecked() && preferences.getBoolean(getString(R.string.checked), false)) {
                     result += "@string/gluten_Allergy";
                 }
 
@@ -49,6 +62,58 @@ public class AccountSettings extends AppCompatActivity {
 
         });
 
+
+        changeLanguageButton = findViewById(R.id.changeLanguageButton);
+        changeLanguageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLanguageDialog();
+            }
+        });
+    }
+
+
+    private void showChangeLanguageDialog() {
+        final String[] listItems = {"@string/French_box", "@string/English_box"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(AccountSettings.this);
+        mBuilder.setTitle(getString(R.string.chooseLang));
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(i ==0) {
+                    setLocale("fr");
+                    Toast.makeText(AccountSettings.this, R.string.eng_sel, Toast.LENGTH_SHORT).show();
+                    recreate();
+                }else if(i ==1) {
+                    setLocale("en");
+                    Toast.makeText(AccountSettings.this, R.string.fr_sel, Toast.LENGTH_SHORT).show();
+                    recreate();
+                }
+                dialog.dismiss();
+            }
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.Settings), MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+        // not sure if it connects properly to mainactivity
+        Intent refresh = new Intent(this,MainActivity.class);
+        startActivity(refresh);
+        finish();
+    }
+    private void loadLocale() {
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.Settings), Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
     }
     public void onCheckboxClicked(View view) {
 
@@ -72,14 +137,7 @@ public class AccountSettings extends AppCompatActivity {
                 editor.putBoolean(getString(R.string.checked), true);
                 editor.apply();
                 break;
-
         }
-
-
         Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
-
-
-
     }
-
 }
